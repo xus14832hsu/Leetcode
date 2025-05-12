79d058c2e11cdc9c514f0c40edad42f7835e1955
Celine Hsu <xus14832@gmail.com>
2025-05-12 23:23:33 +0800

    Create 172.Factorial_Trailing_Zeroes.cpp

---
+++ 172.Factorial_Trailing_Zeroes.cpp
@@ -0,0 +1,12 @@
+//Ãş¦ü 3N+1
+class Solution {
+    public:
+        int trailingZeroes(int n) {
+            int count = 0;
+            while (n >= 5) {
+                count += n / 5;
+                n /= 5;
+            }
+            return count;
+        }
+    };
